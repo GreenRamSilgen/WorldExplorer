59be43a6e4d6dcc5bf2b8d51ff302cb7f85e3571
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-29 07:23:24 -0800

    Initial Commit

---
+++ WorldExplorer/WorldExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldExplorerApp.swift
+//  WorldExplorer
+//
+//  Created by Kiran Shrestha on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct WorldExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
